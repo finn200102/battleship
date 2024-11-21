d15395d7ce17e2cadba274b808a8b4e34952e751
Finn <112250336+finn200102@users.noreply.github.com>
2024-11-21 16:43:00 +0100

    test: getNumberOfHits(), hit()

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -5,4 +5,13 @@ describe("createShip", () => {
     const ship = createShip(3);
     expect(ship.getLength()).toBe(3);
   });
+  test("number of hits", () => {
+    const ship = createShip(3);
+    expect(ship.getNumberOfHits()).toBe(0);
+  });
+  test("add one hit", () => {
+    const ship = createShip(3);
+    ship.hit();
+    expect(ship.getNumberOfHits()).toBe(1);
+  });
 });
