4c176fe10236a09c072c3cdf770b54091f1f3109
Finn <112250336+finn200102@users.noreply.github.com>
2024-11-22 18:55:00 +0100

    test: receiveAttack

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -20,4 +20,12 @@ describe("createShip", () => {
       [0, 0],
     ]);
   });
+  test("recieve attack one ship", () => {
+    const board = createGameboard(2, 2);
+    board.placeShip(0, 0, 0, 1);
+    const boardState = board.getBoard();
+    board.receiveAttack(0, 0);
+    const placedShip = boardState[0][0]; // Get reference to the placed ship
+    expect(placedShip.getNumberOfHits()).toBe(1);
+  });
 });
