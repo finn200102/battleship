6106f0ebffe55d252dccbf65f6d97d06bfb69f45
Finn <112250336+finn200102@users.noreply.github.com>
2024-11-22 14:37:03 +0100

    test: placeShip

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -1,10 +1,22 @@
 import { createGameboard } from "../src/factories/Gameboard";
+import { createShip } from "../src/factories/Ship";
 
 describe("createShip", () => {
   test("create board", () => {
     const board = createGameboard(2, 2);
     expect(board.getBoard()).toStrictEqual([
       [0, 0],
+      [0, 0],
+    ]);
+  });
+  test("place one ship", () => {
+    const board = createGameboard(2, 2);
+    board.placeShip(0, 0, 0, 1);
+    const boardState = board.getBoard();
+    const placedShip = boardState[0][0]; // Get reference to the placed ship
+
+    expect(boardState).toEqual([
+      [placedShip, placedShip], // Use the same reference
       [0, 0],
     ]);
   });
