0a4a66b92047abd037525425b2057e67475e0f2b
Finn <112250336+finn200102@users.noreply.github.com>
2024-11-22 14:37:22 +0100

    add: placeShip

--- src/factories/Gameboard.js
+++ src/factories/Gameboard.js
@@ -1,9 +1,30 @@
+import { createShip } from "./Ship";
+
 export const createGameboard = (rows, cols) => {
-  const board = Array.from({ length: rows }, () => new Array(cols).fill(0));
+  let board = Array.from({ length: rows }, () => new Array(cols).fill(0));
   const getBoard = () => {
     return board;
   };
-  const placeShip = () => {};
+  const placeShip = (startRow, startCol, endRow, endCol) => {
+    const length =
+      Math.abs(endRow - startRow) + Math.abs(endCol - startCol) + 1;
+    const ship = createShip(length);
+    if (startRow == endRow) {
+      // Horizontal placement
+      const minCol = Math.min(startCol, endCol);
+      const maxCol = Math.max(startCol, endCol);
+      for (let col = minCol; col <= maxCol; col++) {
+        board[startRow][col] = ship;
+      }
+    } else if (startCol == endCol) {
+      // Vertical placement
+      const minRow = Math.min(startRow, endRow);
+      const maxRow = Math.max(startRow, endRow);
+      for (let row = minRow; row <= maxRow; row++) {
+        board[row][startCol] = ship;
+      }
+    }
+  };
   const receiveAttack = () => {};
   const allSunk = () => {};
   return {
