6633a41fe3fc718cbe10bf8f1a4eb6a68f7ef210
Finn <112250336+finn200102@users.noreply.github.com>
2024-11-25 18:08:39 +0100

    add: player differentiation

--- src/modules/gameController.js
+++ src/modules/gameController.js
@@ -4,14 +4,14 @@ export const gameController = (() => {
   let player;
   let computer;
   const setUpGame = () => {
-    player = createPlayer();
-    computer = createPlayer();
+    player = createPlayer(10, 10);
+    computer = createPlayer(10, 10);
     player.gameboard.placeShip(0, 0, 0, 1);
     computer.gameboard.placeShip(0, 0, 0, 1);
   };
   const gameLoop = () => {
     setUpGame();
-    displayGameboard.render(player.gameboard.getBoard());
+    displayGameboard.render(player.gameboard.getBoard(), "player");
   };
   return {
     gameLoop,
