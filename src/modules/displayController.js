66cdf2588e837b521d84a585b987d3ffadfabf79
Finn <112250336+finn200102@users.noreply.github.com>
2024-11-25 17:57:31 +0100

    add: displayGameboard.render() - auto square formation

--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -2,9 +2,12 @@ export const displayGameboard = (() => {
   const render = (gameboard) => {
     const boardContainer = document.createElement("div");
     boardContainer.classList.add("gameboard-container");
+    boardContainer.style.gridTemplateRows = `repeat(${gameboard.length}, 0.5fr)`;
+    boardContainer.style.gridTemplateColumns = `repeat(${gameboard[0].length}, 0.5fr)`;
     for (let i = 0; i < gameboard.length * gameboard[0].length; i++) {
       const gameBoardField = document.createElement("div");
       gameBoardField.classList.add("gameboard-field");
+
       boardContainer.appendChild(gameBoardField);
     }
     document.body.appendChild(boardContainer);
