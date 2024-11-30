91eed376908ec44cb0cf7732b77cab03582cc0b1
Finn <112250336+finn200102@users.noreply.github.com>
2024-11-30 16:00:56 +0100

    add: computer moves

--- src/modules/gameController.js
+++ src/modules/gameController.js
@@ -11,19 +11,38 @@ export const gameController = (() => {
   };
   const gameLoop = async () => {
     setUpGame();
-
+    let validMove = true;
     // game loop
     for (let i = 0; i < 10; i++) {
       displayGameboard.render(player.gameboard, "player");
       displayGameboard.render(computer.gameboard, "computer");
-      const playerMoved = await waitForPlayerMove();
-      const [row, col] = computer.gameboard.selectedField();
-      const validMove = computer.gameboard.isValidMove(row, col);
-      if (!validMove) {
-        continue;
+      // player move
+      while (validMove) {
+        const playerMoved = await waitForPlayerMove();
+        const [row, col] = computer.gameboard.selectedField();
+        validMove = computer.gameboard.isValidMove(row, col);
+        if (validMove) {
+          computer.gameboard.receiveAttack(row, col);
+          break;
+        }
       }
-      computer.gameboard.receiveAttack(row, col);
+
+      // computer move
+      while (validMove) {
+        const r = getRandomInt(player.gameboard.getBoard().length);
+        const c = getRandomInt(player.gameboard.getBoard()[0].length);
+        validMove = player.gameboard.isValidMove(r, c);
+
+        if (validMove) {
+          player.gameboard.receiveAttack(r, c);
+          break;
+        }
+      }
     }
+  };
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
   };
   const waitForPlayerMove = () => {
     return new Promise((resolve) => {
