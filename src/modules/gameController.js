6046ec9768c401c9e6a7e2f283203011c371bc8f
Finn <112250336+finn200102@users.noreply.github.com>
2024-11-30 12:24:22 +0100

    add:  wait for player

--- src/modules/gameController.js
+++ src/modules/gameController.js
@@ -9,10 +9,30 @@ export const gameController = (() => {
     player.gameboard.placeShip(0, 0, 0, 1);
     computer.gameboard.placeShip(0, 0, 0, 1);
   };
-  const gameLoop = () => {
+  const gameLoop = async () => {
     setUpGame();
-    displayGameboard.render(player.gameboard.getBoard(), "player");
-    displayGameboard.render(computer.gameboard.getBoard(), "computer");
+
+    // game loop
+    for (let i = 0; i < 10; i++) {
+      displayGameboard.render(player.gameboard, "player");
+      displayGameboard.render(computer.gameboard, "computer");
+      const playerMoved = await waitForPlayerMove();
+      const [row, col] = computer.gameboard.selectedField();
+      const validMove = computer.gameboard.isValidMove(row, col);
+      if (!validMove) {
+        continue;
+      }
+      computer.gameboard.receiveAttack(row, col);
+    }
+  };
+  const waitForPlayerMove = () => {
+    return new Promise((resolve) => {
+      // does not work in gaps
+      const computerBoard = document.getElementById("computer-board");
+      computerBoard.addEventListener("click", () => {
+        resolve(true);
+      });
+    });
   };
   return {
     gameLoop,
