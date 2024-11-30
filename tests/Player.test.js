dbe7258d85d5eee2059e560c5bb5dd4c5c971074
Finn <112250336+finn200102@users.noreply.github.com>
2024-11-30 11:26:10 +0100

    fix: displayGameboard and createPlayer

--- tests/Player.test.js
+++ tests/Player.test.js
@@ -2,7 +2,7 @@ import { createPlayer } from "../src/factories/Player";
 
 describe("createPlayer", () => {
   test("createGameboard", () => {
-    const player = createPlayer();
+    const player = createPlayer(2, 2);
     expect(player.gameboard.getBoard()).toStrictEqual([
       [0, 0],
       [0, 0],
--- tests/displayController.test.js
+++ tests/displayController.test.js
@@ -4,12 +4,12 @@ import { createGameboard } from "../src/factories/Gameboard";
 describe("displayGameboard", () => {
   beforeEach(() => {
     // Clear DOM between tests
-    document.body.innerHTML = "";
+    document.body.innerHTML = "<div id='player-board'></div>";
   });
 
   test("render 2x2 gameboard with 4 cells", () => {
     const gameBoard = createGameboard(2, 2);
-    displayGameboard.render(gameBoard.getBoard());
+    displayGameboard.render(gameBoard.getBoard(), "player");
     const fields = document.querySelectorAll(".gameboard-field");
     expect(fields.length).toBe(4);
   });
